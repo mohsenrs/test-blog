c2ab88647a0ea7df1d17189f9352231c7b972af4
mohsenrs <mohsen8022@gmail.com>
2021-02-27 15:17:45 +0330

    first

---
+++ src/components/PostList.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="post-list">
+    <div v-for="post in posts" :key="post.id">
+      <SinglePost :post="post" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+import SinglePost from "../components/SinglePost";
+export default {
+  props: ["posts"],
+  components: { SinglePost },
+  setup(props) {
+    onMounted(() => {});
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,29 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <h1>Home</h1>
+    <div v-if="error">{{ error }}</div>
+    <div v-if="posts.length">
+      <PostList :posts="posts" />
+    </div>
+    <div v-else>Loading...</div>
+    <!-- <button @click="showPosts = !showPosts">toggle posts</button>
+    <button @click="posts.pop()">delete a post</button> -->
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import PostList from "../components/PostList";
+import getPosts from "../composables/getPosts";
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: { PostList },
+  setup() {
+    const { posts, error, load } = getPosts();
+
+    load();
+
+    return { posts, error };
+  },
+};
 </script>
