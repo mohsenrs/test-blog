f6c98445a5a6ee3bc96001bb0dd5710145b82d74
mohsenrs <mohsen8022@gmail.com>
2021-03-02 18:14:15 +0330

    firebase2

--- src/composables/getPost.js
+++ src/composables/getPost.js
@@ -1,4 +1,5 @@
 import { ref } from "vue";
+import { projectFirestore } from "../firebase/config";
 
 const getPost = (id) => {
   const post = ref(null);
@@ -6,16 +7,14 @@ const getPost = (id) => {
 
   const load = async () => {
     try {
-      //Delay
-      await new Promise((resolve) => {
-        setTimeout(resolve, 2000);
-      });
-
-      let data = await fetch("http://localhost:3000/posts/" + id);
-      if (!data.ok) {
-        throw Error("post does not exist");
+      let res = await projectFirestore
+        .collection("posts")
+        .doc(id)
+        .get();
+      if (!res.exists) {
+        throw Error("That post does not exist");
       }
-      post.value = await data.json();
+      post.value = { ...res.data(), id: res.id };
     } catch (err) {
       error.value = err.message;
     }
--- src/views/Create.vue
+++ src/views/Create.vue
@@ -16,6 +16,7 @@
 <script>
 import { ref } from "vue";
 import { useRouter } from "vue-router";
+import { projectFirestore } from '../firebase/config';
 
 export default {
   setup() {
@@ -40,11 +41,7 @@ export default {
         body: body.value,
         tags: tags.value,
       };
-      await fetch("http://localhost:3000/posts", {
-        method: "POST",
-        headers: { "Content-Type": "application/json" },
-        body: JSON.stringify(post),
-      });
+      const res = await projectFirestore.collection('posts').add(post)
       router.push({ name: "Home" });
     };
 
